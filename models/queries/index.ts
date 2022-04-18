048fad896672ee1a9b8e3805e9d01e1db57e233a
Abhishek Malviya <71405686+malviya-abhishek@users.noreply.github.com>
2022-04-19 01:58:49 +0530

    bugged code

--- models/queries/index.ts
+++ models/queries/index.ts
@@ -1,12 +1,16 @@
 import * as users from "./users.query";
 import * as roles from "./roles.query";
+import { QueryConfig } from "pg";
 
 
-export const CREATE_TABLES = {
-  text: roles.CREATE_ROLES + users.CREATE_USERS
-};
+export const CREATE_TABLES = (): QueryConfig<any[]> => ({
+  text: roles.CREATE_ROLES_TABLE() + users.CREATE_USERS_TABLE()
+});
 
-export const GENERATE_DATA = {
-  text: roles.GENERATE_ROLES
-}
+export const GENERATE_DATA = () : QueryConfig => ({
+  text: roles.GENERATE_ROLES()
+});
 
+export const CREATE_USER = (user: any): QueryConfig<any> => ({
+  text: users.CREATE_USER(user),
+});

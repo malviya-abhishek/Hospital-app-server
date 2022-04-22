2543bf5a6bda08984e88c2d8b631f73ffd1dc7c4
Abhishek Malviya <71405686+malviya-abhishek@users.noreply.github.com>
2022-04-22 19:44:32 +0530

    users ready

---
+++ routes/patient.routes.ts
@@ -0,0 +1,9 @@
+import { Express, Request, Response } from "express";
+import * as userController from '../controller/user.controller';
+
+export const patientRoutes = function(app: Express){
+  // app.get("/users", [userController.getUsers]);
+  // app.get("/users/:userId", [userController.getUser])
+  // app.post("/users", [userController.postUser]);
+  app.post("/appointment/", )
+}
